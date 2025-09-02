1a8504c4a15fbcb1eeee73e94e2564ea75da4fd8
Simone <simoneilagan@outlook.com>
2025-09-02 14:44:42 -0400

    moved background out of pomodoro comp

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { PomodoroComponent } from './pomodoro/pomodoro.component';
+import { ColorDirective } from './color.directive';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [PomodoroComponent],
+  imports: [PomodoroComponent, ColorDirective],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'pomomomo';
+  title = 'PomoMomo';
 }
