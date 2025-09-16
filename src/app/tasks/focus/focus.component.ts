c1d5c82e031327a2276ecc1c244c10a9e9b71069
Simone <simoneilagan@outlook.com>
2025-09-16 12:56:47 -0400

    feat(tasks): init commit

---
+++ src/app/tasks/focus/focus.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-focus',
+  imports: [],
+  templateUrl: './focus.component.html',
+  styleUrl: './focus.component.css'
+})
+export class FocusComponent {
+
+}
