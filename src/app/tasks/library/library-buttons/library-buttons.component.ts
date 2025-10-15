703be2c7611008b2e14bea69c1e423407046346e
Simone <simoneilagan@outlook.com>
2025-10-15 16:34:20 -0400

    fix(lib-buttons): remove sort and delete overlap on click event.

--- src/app/tasks/library/library-buttons/library-buttons.component.ts
+++ src/app/tasks/library/library-buttons/library-buttons.component.ts
@@ -11,11 +11,10 @@ import { TasksService } from '../../tasks.service';
 export class LibraryButtonsComponent {
   private tasksService = inject(TasksService);
 
-  onClick(type: string) {
-    if (type === 'category' || type === 'status' || type === 'dateCreation') {
-      this.tasksService.sortTasksList(type);
-    } else if (type === 'all' || type === 'completed') {
-      this.tasksService.deleteTasksList(type);
-    }
+  onClickSort(type: string) {
+    this.tasksService.sortTasksList(type);
+  }
+  onClickDelete(type: string) {
+    this.tasksService.deleteTasksList(type);
   }
 }
