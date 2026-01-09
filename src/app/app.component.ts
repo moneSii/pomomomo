6372f1bd5862dfd7b7d1a287a13332cf60ea1079
Simone <simoneilagan@outlook.com>
2026-01-09 09:56:47 -0500

    refactor(app): remove routing.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,15 @@
 import { Component, inject } from '@angular/core';
 
 import { BackgroundComponent } from './background/background.component';
-
-import { RouterOutlet } from '@angular/router';
+import { PomodoroComponent } from './pomodoro/pomodoro.component';
+import { TasksComponent } from './tasks/tasks.component';
 
 import { DisplayService } from './display.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, BackgroundComponent],
+  imports: [BackgroundComponent, PomodoroComponent, TasksComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,9 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
-import { routes } from './app.routes';
 import { provideAnimations } from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: false }),
     provideAnimations(),
-    provideRouter(routes),
   ],
 };
